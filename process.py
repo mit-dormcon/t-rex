62a6f0dc418f867bf4dc4091aab422607d576cdb
Cameron Kleiman <camtheman256@gmail.com>
2022-05-28 16:57:56 -0400

    add full list of tags response

--- process.py
+++ process.py
@@ -46,6 +46,7 @@ if __name__ == "__main__":
     
     api_response["events"].sort(key=lambda e: e["start"])
     api_response["dorms"] = sorted(list(set(e["dorm"] for e in api_response["events"])))
+    api_response["tags"] = sorted(list(set(t for e in api_response["events"] for t in e["tags"])))
 
     print("Processing complete! Creating API JSON...")
     
